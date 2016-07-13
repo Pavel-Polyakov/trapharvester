da86dd18e03da1bfa693dafffb911049fad99976
Pavel Polyakov <woolly@noc.ihome.ru>
2016-07-14 00:52:18 +0300

    addded supporting of several addresses

--- mailer.py
+++ mailer.py
@@ -8,13 +8,16 @@ from email.mime.text import MIMEText
 from config import MAIL_FROM
 
 def send_mail(subject,to_address,text):
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = MAIL_FROM
-    msg['To'] = to_address
-    part1 = MIMEText(text.encode('utf-8'), 'html', 'utf-8')
-    msg.attach(part1)
-    server = smtplib.SMTP('localhost')
-    server.sendmail(MAIL_FROM,to_address,msg.as_string())
-    server.close()
+    if type(to_address) != list:
+        to_address = [to_address]
+    for address in to_address:
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = MAIL_FROM
+        msg['To'] = address
+        part1 = MIMEText(text.encode('utf-8'), 'html', 'utf-8')
+        msg.attach(part1)
+        server = smtplib.SMTP('localhost')
+        server.sendmail(MAIL_FROM,address,msg.as_string())
+        server.close()
 
