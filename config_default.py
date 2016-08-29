7e06a3dc4974843891046ade19fc57dc4bfe702b
Pavel Polyakov <woolly@noc.ihome.ru>
2016-08-29 16:41:59 +0300

    added default config file

---
+++ config_default.py
@@ -0,0 +1,7 @@
+DB_URL = 'mysql+pymysql://trap:0o9i8u@localhost/traps'
+MAIL_FROM = 'harvester@noc.ihome.ru'
+MAIL_TO = 'traps@ihome.ru'
+# MAIL_TO = ['woolly@ihome.ru']
+SNMP_COMMUNITY = 'qwsxczasde'
+FLAP_THR_MINUTES = 15
+FLAP_THR_COUNT = 20
