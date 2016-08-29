916706dfda478e2032be78b52550d993390a090e
Pavel Polyakov <woolly@noc.ihome.ru>
2016-08-29 16:51:20 +0300

    changed default config file

--- config_default.py
+++ config_default.py
@@ -1,7 +1,6 @@
-DB_URL = 'mysql+pymysql://trap:0o9i8u@localhost/traps'
-MAIL_FROM = 'harvester@noc.ihome.ru'
-MAIL_TO = 'traps@ihome.ru'
-# MAIL_TO = ['woolly@ihome.ru']
-SNMP_COMMUNITY = 'qwsxczasde'
+DB_URL = 'mysql+pymysql://flap:flapmyport@localhost/traps'
+MAIL_FROM = 'trap_harvester@yourdomain.com'
+MAIL_TO = ['user_first@yourdomain.com','user_second@yourdomain.com']
+SNMP_COMMUNITY = 'your_SNMP_community'
 FLAP_THR_MINUTES = 15
 FLAP_THR_COUNT = 20
