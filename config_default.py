0b3545cb2723c7381291362bfa91b2759e94c013
Pavel-Polyakov <polyakov.pavel.xyz@gmail.com>
2016-09-01 17:46:13 +0300

    Update config_default.py

--- config_default.py
+++ config_default.py
@@ -1,7 +1,17 @@
+# configure it for your mysql database
+# your_username: instead 'flap'
+# your_server: instead 'localhost'
+# your_database: instead 'traps'
 DB_URL = 'mysql+pymysql://flap:flapmyport@localhost/traps'
 MAIL_FROM = 'trap_harvester@yourdomain.com'
 MAIL_TO = ['user_first@yourdomain.com','user_second@yourdomain.com']
+
+# it's neede for getting description of interface and hostname of device
 SNMP_COMMUNITY = 'your_SNMP_community'
+
+# flapping interface flaps more then FLAP_THR_COUNT in the last FLAP_THR_MINUTES
 FLAP_THR_MINUTES = 15
 FLAP_THR_COUNT = 20
-TRANSLATE = False
+
+# set it to True for receiving mails in Russian
+TRANSLATE = False 
